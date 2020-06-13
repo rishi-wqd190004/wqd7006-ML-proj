6e843a37a93f5441f78064991e97a9e26089c054
Owen Williams <owen.williams@live.co.uk>
2020-06-13 21:33:39 +0800

    Read strings from input and predict

--- rishi_sarcasam.py
+++ rishi_sarcasam.py
@@ -5,6 +5,7 @@ from nltk.stem.porter import PorterStemmer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.svm import LinearSVC
+from sklearn.calibration import CalibratedClassifierCV
 from sklearn.naive_bayes import GaussianNB
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
@@ -36,7 +37,7 @@ features = tv.fit_transform(features).toarray()
 features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=.05, random_state=0)
 
 #model 1: using linear SVM
-lsvc = LinearSVC()
+lsvc = CalibratedClassifierCV(base_estimator=LinearSVC(penalty='l2', dual=False), cv=5)
 lsvc.fit(features_train, labels_train)
 print("train score for lsvc",lsvc.score(features_train, labels_train))
 print("test score for lsvc",lsvc.score(features_test, labels_test))
@@ -59,13 +60,14 @@ rfc.fit(features_train, labels_train)
 print("train score for random forest", rfc.score(features_train, labels_train))
 print("test score for random forest", rfc.score(features_test, labels_test))
 
+
 #predict 1: using Linear SVM model
 data = pd.read_csv('friends_dataset.csv', dtype={'Text': 'str'})
 data.dropna(inplace=True)
 print(data.info())
 print(data.head())
 print(data.isnull().any(axis=0))
-features = data['Text'].apply(lambda s : re.sub('[^a-zA-Z]', ' ', s))
+features = data['Text'].apply(lambda s: re.sub('[^a-zA-Z]', ' ', s))
 features = features.apply(lambda x: x.split())
 features = features.apply(lambda x: ' '.join([ps.stem(word) for word in x]))
 features = list(features)
@@ -73,5 +75,31 @@ features = tv.fit_transform(features).toarray()
 data['sarcastic'] = lsvc.predict(features)
 data = data.replace({0: 'non-sarcastic', 1: 'sarcastic'})
 data = data.groupby('Speaker')['sarcastic'].value_counts().unstack().fillna(0)
-dfCounts['sarcasm ratio'] = dfCounts['non-sarcastic'] / dfCounts['sarcastic']
-print(dfCounts)
+data['sarcasm ratio'] = data['non-sarcastic'] / data['sarcastic']
+print(data)
+
+#predict 2: take command-line input and predict using model
+print("##########################################")
+print()
+print("How sarcastic are you?!")
+print()
+print("##########################################")
+print()
+while(True):
+    print("Tell me something ...")
+    inputString = str(input())
+    data = pd.DataFrame({'Text': [inputString]}, dtype=str)
+    data.dropna(inplace=True)
+    # print(data.info())
+    # print(data.head())
+    # print(data.isnull().any(axis=0))
+    features = data['Text'].apply(lambda s: re.sub('[^a-zA-Z]', ' ', s))
+    features = features.apply(lambda x: x.split())
+    features = features.apply(lambda x: ' '.join([ps.stem(word) for word in x]))
+    features = list(features)
+    features = tv.transform(features).toarray()
+    sarcasm = lsvc.predict_proba(features)[:,1][0]
+    sarcasm = sarcasm * 100
+    sarcasm = "{:.2f}".format(sarcasm)
+    print("That was " + sarcasm + "% sarcastic!")
+    print()
